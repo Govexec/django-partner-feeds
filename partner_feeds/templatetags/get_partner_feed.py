28aa8309d5c54fe2b45b56ca4ba4ddcc2770d7d5
Llewellyn Hinkes-Jones <lhinkesjones@theatlantic.com>
2013-05-06 13:26:19 -0400

    Adding optional ignored_titles keyword

--- partner_feeds/templatetags/get_partner_feed.py
+++ partner_feeds/templatetags/get_partner_feed.py
@@ -4,14 +4,17 @@ from partner_feeds.models import Post
 register = template.Library()
 
 @register.object
-def get_partner_feeds(partner_names=[], partner_ids=[], num=3):
+def get_partner_feeds(partner_names=[], partner_ids=[], ignored_titles=[], num=3):
 
 	partners = []
 
 	if len(partner_ids) > 0:
 
 		for id in partner_ids:
-			posts = list(Post.objects.filter(partner__id=id).select_related('partner').order_by("-date")[:num])
+			qs = Post.objects.filter(partner__id=id).select_related('partner').order_by("-date")
+			if ignored_titles:
+				qs.exclude(title__in=ignored_titles)
+			posts = list(qs[:num])
 			if len(posts) > 0:
 				partner = posts[0].partner
 				partner.posts = posts
@@ -20,7 +23,12 @@ def get_partner_feeds(partner_names=[], partner_ids=[], num=3):
 	elif len(partner_names) > 0:
 
 		for name in partner_names:
-			posts = list(Post.objects.filter(partner__name=name).select_related('partner').order_by("-date")[:num])
+			qs = Post.objects.filter(partner__name=name).select_related('partner').order_by("-date")
+			if ignored_titles:
+				qs.exclude(title__in=ignored_titles)
+				
+			posts = list(qs[:num])
+			
 			if len(posts) > 0:
 				partner = posts[0].partner
 				partner.posts = posts
